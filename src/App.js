62a9422b57d98083c815c22a4bdb47210f327535
James Zuckerman <jzuckerman4@gmail.com>
2019-04-08 19:41:02 -0700

    Data from api is displayed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import LiveStreams from './components/LiveStreams';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <LiveStreams />;
   }
 }
 
--- src/components/LiveStreams.js
+++ src/components/LiveStreams.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
-const twitchAPIUrl = 'https://api.twitch.tv/kraken/';
+const twitchAPIUrl = 'https://api.twitch.tv/kraken/streams?channel=';
+const channelNames =
+  'mizkif,ninja,destiny,xQcOW,Hyphonix,Boneclinks,shroud,DrDisrespect,LIRIK,AndyMilonakis,Greekgodx,trihex,cloakzy,Tfue,Reckful,Asmongold,erobb221,Yassuo,Trainwreckstv,sodapoppin,MitchJones,Nmplol,summit1g,loltyler1,pokelawls,ZeRo,CoconutB';
 const CLIENT_ID = 'qc6qmzy5llaqzvfagxyjw7ekzpzs3s';
 
 class LiveStreams extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liveStreams: null
+    };
+  }
+  async componentDidMount() {
+    const response = await fetch(
+      twitchAPIUrl + channelNames + '&client_id=' + CLIENT_ID
+    );
+
+    const streamData = await response.json();
+    console.log(streamData);
+    this.setState({ liveStreams: streamData.streams });
+  }
+
   render() {
-    return <div className="App" />;
+    if (this.state.liveStreams != null) {
+      return (
+        <div>
+          {this.state.liveStreams.map(stream => (
+            <div>
+              <h2>{stream.channel.display_name}</h2>
+              <a href={stream.channel.url}>Watch Now</a>
+              <h2>{stream.viewers}</h2>
+              <h2>{stream.channel.game}</h2>
+              <img src={stream.channel.logo} />
+              <img src={stream.preview.large} />
+            </div>
+          ))}
+        </div>
+      );
+    } else return <h1>hi</h1>;
   }
 }
 
