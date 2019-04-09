ab93f441524a4b1b8eb6d1351fb8b440cc65eff9
James Zuckerman <jzuckerman4@gmail.com>
2019-04-08 17:42:38 -0700

    Initialization

---
+++ src/components/LiveStreams.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const twitchAPIUrl = 'https://api.twitch.tv/kraken/';
+const CLIENT_ID = 'qc6qmzy5llaqzvfagxyjw7ekzpzs3s';
+
+class LiveStreams extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default LiveStreams;
